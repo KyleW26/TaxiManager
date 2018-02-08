fda69cbf8e7d059882adfcdcd570a18575e6a7f4
SpaceDux <ryan.a.williams@outlook.com>
2018-02-08 23:53:41 +0000

    Basic template for Kyle
    Learning the fenrir template engine

---
+++ template/2018/index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<HTML>
+  <head>
+    <title>{SITE_NAME} | Hub </title>
+    <link href="https://fonts.googleapis.com/css?family=Mukta+Mahee" rel="stylesheet">
+    <link rel="stylesheet" href="{URL}/template/{TPL}/assets/css/style.css">
+  </head>
+  <body>
+    <nav>
+      <div class="content">
+        <div class="Options">
+          <div class="Option">
+            <a href="{URL}/index"> Home </a>
+          </div>
+          <div class="Option">
+            <a href="{URL}/index"> About </a>
+          </div>
+          <div class="Option">
+            <a href="{URL}/index"> Contact </a>
+          </div>
+          <div class="Option">
+            <a href="{URL}/index"> Another Link </a>
+          </div>
+          <div class="Option">
+            <a href="{URL}/index"> fuck it, have a biggun </a>
+          </div>
+        </div>
+        </div>
+      </div>
+    </nav>
+    <div class="ContentBox">
+      <h1>Hello world</h1>
+      <p>This is a basic template for Kyles {SITE_NAME}, it's located at {URL}/template/{TPL} <---</p>
+    </div>
+  </body>
+</HTML>
