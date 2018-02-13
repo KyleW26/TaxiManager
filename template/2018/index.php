ec25c0168df67b2d2709da47c3097cf95602c90a
Kyle <eeu8e1@bangor.ac.uk>
2018-02-13 21:58:44 +0000

    Added icons
    FontAwesome update.

--- template/2018/index.php
+++ template/2018/index.php
@@ -3,29 +3,53 @@
   <head>
     <title>{SITE_NAME} | Hub </title>
     <link href="https://fonts.googleapis.com/css?family=Mukta+Mahee" rel="stylesheet">
-    <link rel="stylesheet" href="{URL}/template/{TPL}/assets/css/style.css">
+    <link rel="stylesheet" type="text/css" href="{URL}/template/{TPL}/assets/css/style.css">
+    <link rel="stylesheet" type="text/css" href="{URL}/template/{TPL}/assets/css/fontawesome-all.css">
     <link rel="icon" href="{URL}/img/cab.png">
   </head>
   <body>
-    <!-- <img src="{URL}/img/MainLogo.jpg" alt="Mountain View" width="250" height="70"> -->
-    <nav>
 
+    <nav>
     </nav>
 
     <div class="sidePanel">
-
+      <div class="profileBox">
+        <p><center> Welcome *USERNAME* </center></p>
+            <center><img src="{URL}/img/avatar.png" width="128" height="128" class="avatar"><center>
+      </div>
     </div>
 
     <div class="currentTaxiBox">
+      <div class="awesomeIcons">
+        <i class="fas fa-taxi" style="font-size:80px"></i>
+      </div>
+
+      <div class="contentText">
+        <h1> Current Cars </h1>
+      </div>
 
     </div>
 
     <div class="regularBookings">
+      <div class="awesomeIcons">
+        <i class="fas fa-clock" style="font-size:80px"></i>
+      </div>
+
+      <div class="contentText">
+        <h1>Regular Bookings</h1>
+      </div>
 
     </div>
 
     <div class="contentBox3">
-      
+      <div class="awesomeIcons">
+        <i class="fas fa-user" style="font-size:80px"></i>
+      </div>
+
+      <div class="contentText">
+        <h1> Current Operator </h1>
+      </div>
+
     </div>
 
   </body>
